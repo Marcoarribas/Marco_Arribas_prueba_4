6d07d899f40ade2273dab2f8323cd6edea8895f4
Marco Arribas <marco.arribas@alumni.mondragon.edu>
2024-05-28 18:01:03 +0200

    ultimisimos cambios

--- API_examen_Marco_Arribas.R
+++ API_examen_Marco_Arribas.R
@@ -1,13 +1,20 @@
 #* @apiTitle API examen Marco Arribas
 #* @apiDescription API que permite a los usuarios buscar las viviendas que cumplen ciertas condiciones
-#* @param precio:int El precio máximo de la vivienda.
-#* @param tipo_residencia:str El tipo de residencia (e.g., "Apartamento", "Casa").
+#* @param precio El precio máximo de la vivienda
+#* @param tipo_residencia El tipo de residencia
 #* @get /busqueda_piso
 function(precio, tipo_residencia) {
-  library(dplyr)
-  df <- read.csv("real_state.csv")
+  # Leer el archivo CSV
+  df <- read.csv("real_estate.csv")
+  
+  # Convertir precio a numérico (en caso de que venga como string)
+  precio <- as.numeric(precio)
+  
+  # Filtrar las viviendas según los parámetros proporcionados
   viviendas <- df %>% 
     filter(Assessed.Value < precio, Property.Type == tipo_residencia) %>%
-    select(ID, Address, Assessed.Value)
+    dplyr::select(ID, Address, Assessed.Value)
+  
+  # Devolver las viviendas filtradas
   return(viviendas)
 }
